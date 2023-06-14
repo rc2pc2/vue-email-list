b42a5ab568be014036e7a3f82d048be854a15eb7
rc2pc2 <petriccarcc@gmail.com>
2023-06-14 09:50:37 +0200

    created two more generic methods to handle email generation

--- js/script.js
+++ js/script.js
@@ -4,22 +4,27 @@ createApp({
     data() {
         return {
             emailList : [],
+            numberOfEmails: 0,
         }
     },
 
     methods:{
+        addNewEmail(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (result) => {
+                this.emailList.push(result.data.response);
+            });
+        },
 
+        generateEmailList(numberOfEmails){
+            this.numberOfEmails = numberOfEmails;
+            for (let index = 0; index < numberOfEmails; index++) {
+                this.addNewEmail();
+            }
+        }
     },
 
     created(){
-        for (let index = 0; index < 10; index++) {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then( (result) => {
-                console.log(result.data.response);
-                email = result.data.response;
-                this.emailList.push(email)
-            });
-        }
-
+        this.generateEmailList(10);
     }
 }).mount('#app');
