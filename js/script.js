56376a691b1158a702332e1c7b31cca3b33e2aa1
rc2pc2 <petriccarcc@gmail.com>
2023-06-14 09:39:35 +0200

    api first testing implementation

--- js/script.js
+++ js/script.js
@@ -3,17 +3,18 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            emailList : [],
         }
     },
 
     methods:{
 
-
     },
 
     created(){
-
-
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( (result) => {
+            console.log(result);
+        });
     }
 }).mount('#app');
